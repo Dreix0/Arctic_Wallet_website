db49db11b4acf088b129262a190ddbcc8b2706a3
Dreix0 <quentin.denolle2001@gmail.com>
2024-07-03 19:44:20 +0200

    display faq question

--- app/[locale]/components/faq.tsx
+++ app/[locale]/components/faq.tsx
@@ -7,13 +7,22 @@ const FAQ = (() => {
 
     const [displayResponse, setDisplayResponse] = useState(0);
 
+    function changeQuestion(question: number){
+        if(displayResponse == question){
+            setDisplayResponse(0);
+        }
+        else {
+            setDisplayResponse(question);
+        }
+    }
+
     return (
       <>
         <div id="faq_link"></div>
         <div id="faq">
             <h1>FAQ</h1>
             <div>
-            <h2 onClick={() => setDisplayResponse(1)}>{t("question_1")}</h2>
+            <h2 onClick={() => changeQuestion(1)}>{t("question_1")}</h2>
             {displayResponse == 1 ? <div>
                 <p>{t("answer_1__p1")}</p>
                 <ul>
@@ -23,7 +32,7 @@ const FAQ = (() => {
                 <p>{t("answer_1__p2")}</p>
                 </div> : <></>}
             <hr/>
-            <h2 onClick={() => setDisplayResponse(2)}>{t("question_2")}</h2>
+            <h2 onClick={() => changeQuestion(2)}>{t("question_2")}</h2>
             {displayResponse == 2 ? <div>
                 <p>{t("answer_2__p1")}</p>
                 <ul>
@@ -38,17 +47,17 @@ const FAQ = (() => {
                 <p>{t("answer_2__p3")}</p>
             </div> : <></>}
             <hr/>
-            <h2 onClick={() => setDisplayResponse(3)}>{t("question_3")}</h2>
+            <h2 onClick={() => changeQuestion(3)}>{t("question_3")}</h2>
             {displayResponse == 3 ? <div>
                 <p>{t("answer_3__p1")}<br/><br/>{t("answer_3__p2")}<br/><br/>{t("answer_3__p3")}</p>
             </div> : <></>}
             <hr/>
-            <h2 onClick={() => setDisplayResponse(4)}>{t("question_4")}</h2>
+            <h2 onClick={() => changeQuestion(4)}>{t("question_4")}</h2>
             {displayResponse == 4 ? <div>
                 <p>{t("answer_4__p1")}<br/><br/>{t("answer_4__p2")}<br/><br/>{t("answer_4__p3")}</p>
             </div> : <></>}
             <hr/>
-            <h2 onClick={() => setDisplayResponse(5)}>{t("question_5")}</h2>
+            <h2 onClick={() => changeQuestion(5)}>{t("question_5")}</h2>
             {displayResponse == 5 ? <div>
                 <p>{t("answer_5__p1")}<br/><br/>{t("answer_5__p2")}</p>
             </div> : <></>}
